874430ac1f9d1292a204214ded7bec039cc4ffc7
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-09-06 08:30:25 +0100

    Solve exercise 3.4

--- index.js
+++ index.js
@@ -51,7 +51,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 });
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(person => person.id !== id);
 
+    response.status(204).end();
+});
 
 
 
