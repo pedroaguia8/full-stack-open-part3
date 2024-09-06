8b26955e221329708dd4ccf134d16f1ddd4cc944
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-09-06 08:24:27 +0100

    Solve exercise 3.3

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ app.get('/info', (request, response) => {
     response.send(infoPage);
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
+
 
 
 
